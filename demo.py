7f81f2c0115aa8f54ae15dcb801679376f89fbba
Aqeeb27401 <77690409+Aqeeb27401@users.noreply.github.com>
2022-06-14 09:39:40 +0500

    Update demo.py
    put condition statement but run the until user enter 2

--- demo.py
+++ demo.py
@@ -1,10 +1,13 @@
 import mysql.connector as c
 con=c.connect(host="localhost",user="root",passwd="",database="aqeeb")
 Cursor=con.cursor()
-code=int(input("Enter code:"))
-name=input("Enter name:")
-design=input("Enter designation:")
-salary=int(input("Enter salary:"))
-Cursor.execute("INSERT INTO `emp` (`code`, `name`, `design`, `salary`) VALUES (NULL, '{}', '{}', '{}');".format(code,name,design,salary))
-con.commit()
-print("Successfully Entered")
+while True:
+   name=input("Enter name:")
+   design=input("Enter designation:")
+   salary=int(input("Enter salary:"))
+   Cursor.execute("INSERT INTO `emp` (`code`, `name`, `design`, `salary`) VALUES (NULL, '{}', '{}', {});".format(name,design,salary))
+   con.commit()
+   print("Successfully Entered")
+   x=int(input("1-->Continuoue and 2-->Exit")
+   if x==2:
+        break
